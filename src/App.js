131a954a7e6e1535ba053e4f1a246c679754240d
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-07-29 12:50:27 +0300

    feat/setting i18n

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
+import { Suspense } from 'react';
 import './scss/app.scss';
 import Header from './componets/Header';
 import Categories from './componets/Categories';
 import Sort from './componets/Sort';
 import PizzaBlock from './componets/PizzaBlock';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock title="Мексіканська" price={500} />
-            <PizzaBlock />
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock title="Мексіканська" price={500} />
+              <PizzaBlock />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
 
